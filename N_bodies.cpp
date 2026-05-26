f4b684c8a50b1aab34a28e8a5c2e6d4f0618094d
sofiaromani469 <sofiaromani469@gmail.com>
2026-05-26 15:59:56 +0200

    Modified N_bodies

--- N_bodies.cpp
+++ N_bodies.cpp
@@ -1,1 +1,18 @@
 #include "N_bodies.hpp"
+#include <cmath>
+#include <vector>
+#include <iostream>
+class Planet
+{
+    double mass{};
+    double pos_x{};
+    double pos_y{};
+    double vel_x{};
+    double vel_y{};
+    double acc_x{};
+    double acc_y{};
+
+    double const epsilon{pow(10, -12)};
+};
+
+std::vector<Planet> Bodies;
