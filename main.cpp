339ab98c576c2ca15ac65c83d07086db9fb8e6ac
sofiaromani469 <sofiaromani469@gmail.com>
2026-08-02 21:48:59 +0200

    marameo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <algorithm>
+#include <SFML/Graphics.hpp>
 #include "N_bodies.hpp"
+
 int main()
 {
   Simulation solar_system;
@@ -16,9 +18,6 @@ int main()
   }
   solar_system.initAccelerations();
 
-  double dt = 3600.0;
-  int n_steps = 24 * 365;
-
   double initEnergy = solar_system.consEnergy();
   std::cout << "Energia meccanica iniziale: " << initEnergy << '\n';
   solar_system.energiesHistory.push_back(initEnergy);
@@ -31,29 +30,63 @@ int main()
   std::cout << "Quantità di moto iniziale: " << initMomentum << '\n';
   solar_system.momentumHistory.push_back(initMomentum);
 
-  for (int i = 0; i < n_steps; ++i)
+  double dt = 3600.0;
+  double scale = 350.0 / 1.496e11;
+
+  sf::RenderWindow window(sf::VideoMode(800, 600), "N-Body Simulation");
+
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+    for (int k = 0; k < 10; k++)
+    {
+      solar_system.step(dt);
+    }
+    window.clear(sf::Color::Black);
+
+    for (auto &body : solar_system.bodies)
+    {
+      sf::CircleShape circle(6.f); // raggio grafico fisso
+      circle.setFillColor(sf::Color::White);
+      circle.setOrigin(6.f, 6.f); // centra il cerchio sul punto
+
+      float screenX = 400 + body.position.x * scale;
+      float screenY = 300 + body.position.y * scale;
+      circle.setPosition(screenX, screenY);
+
+      window.draw(circle);
+    }
+
+    window.display();
+  }
+  /*for (int i = 0; i < n_steps; ++i) Questo ciclo senza sfml NON eliminiamolo che poi vediamo come implementare i cout.
   {
     solar_system.step(dt);
     if (i % (24 * 30) == 0)
     {
-      /*std::cout << "t = " << (i * dt) / 86400.0 << " giorni | Terra: (" << solar_system.bodies[1].position.x << ", " << solar_system.bodies[1].position.y << ")\n";
+      std::cout << "t = " << (i * dt) / 86400.0 << " giorni | Terra: (" << solar_system.bodies[1].position.x << ", " << solar_system.bodies[1].position.y << ")\n";
       std::cout << "velocità sole: " << solar_system.bodies[0].velocity.module(solar_system.bodies[0].velocity) << '\n';
-      std::cout << "velocità terra: " << solar_system.bodies[1].velocity.module(solar_system.bodies[1].velocity) << '\n';*/
+      std::cout << "velocità terra: " << solar_system.bodies[1].velocity.module(solar_system.bodies[1].velocity) << '\n';
       std::cout << "t = " << (i * dt) / 86400.0 << " giorni | numero corpi: " << solar_system.bodies.size() << '\n';
 
-  if (solar_system.bodies.size() > 1)
-  {
-    std::cout << "Terra: (" << solar_system.bodies[1].position.x << ", " << solar_system.bodies[1].position.y << ")\n";
-    std::cout << "velocità sole: " << solar_system.bodies[0].velocity.module(solar_system.bodies[0].velocity) << '\n';
-    std::cout << "velocità terra: " << solar_system.bodies[1].velocity.module(solar_system.bodies[1].velocity) << '\n';
-  }
-  else
-  {
-    std::cout << "Fusione avvenuta -> massa corpo unico: " << solar_system.bodies[0].getMass() << '\n';
-  }
+      if (solar_system.bodies.size() > 1)
+      {
+        std::cout << "Terra: (" << solar_system.bodies[1].position.x << ", " << solar_system.bodies[1].position.y << ")\n";
+        std::cout << "velocità sole: " << solar_system.bodies[0].velocity.module(solar_system.bodies[0].velocity) << '\n';
+        std::cout << "velocità terra: " << solar_system.bodies[1].velocity.module(solar_system.bodies[1].velocity) << '\n';
+      }
+      else
+      {
+        std::cout << "Fusione avvenuta -> massa corpo unico: " << solar_system.bodies[0].getMass() << '\n';
+      }
       std::cout << "Energia meccanica: " << solar_system.consEnergy() << '\n';
     }
-  }
+  }*/
   double maxEnergy = *std::max_element(std::begin(solar_system.energiesHistory), std::end(solar_system.energiesHistory));
   double minEnergy = *std::min_element(std::begin(solar_system.energiesHistory), std::end(solar_system.energiesHistory));
   std::cout << "L'energia oscilla tra " << maxEnergy << " e " << minEnergy << '\n';
@@ -65,8 +98,4 @@ int main()
   double maxMomentum = *std::max_element(std::begin(solar_system.momentumHistory), std::end(solar_system.momentumHistory));
   double minMomentum = *std::min_element(std::begin(solar_system.momentumHistory), std::end(solar_system.momentumHistory));
   std::cout << "la quantità di moto oscilla tra " << maxMomentum << " e " << minMomentum << '\n';
-  std::cout << "la massa del corpo uno è" << solar_system.bodies[0].getMass() << '\n';
-  if(solar_system.bodies.size()>1){
-  std::cout << "la massa del corpo due è" << solar_system.bodies[1].getMass() << '\n';}
-
 }
