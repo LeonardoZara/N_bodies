6888c1afbd031d4b502ffafa7e12ef485e2726e5
sofiaromani469 <sofiaromani469@gmail.com>
2026-08-16 21:14:57 +0200

    random engine modificato

--- n_bodies.hpp
+++ n_bodies.hpp
@@ -75,7 +75,7 @@ namespace nb
         static constexpr double G{6.6743e-11};
         static constexpr double pi = 3.14159265358979323846;
         std::default_random_engine eng;
-        std::uniform_real_distribution<int> uniform{2, 6};
+        std::uniform_int_distribution<int> uniform{2, 6};
         std::vector<Planet> bodies;
 
     public:
